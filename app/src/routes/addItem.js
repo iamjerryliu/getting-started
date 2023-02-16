7c896261779345c091a8dceed587d19ae99ca8f7
iamjerryliu <iamjerryliu@gmail.com>
2023-02-16 23:20:40 +0800

    Test commite 2

--- app/src/routes/addItem.js
+++ app/src/routes/addItem.js
@@ -5,12 +5,12 @@ module.exports = async (req, res) => {
     const item = {
         id: uuid(),
         name: req.body.name,
-        
+
         completed: false,
     };
 
     await db.storeItem(item);
 
-
+     
     res.send(item);
 };
